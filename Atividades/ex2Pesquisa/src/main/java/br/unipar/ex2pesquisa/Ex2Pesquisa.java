a5b6418edf7c08add88a6745d1e0adb52eab39a3
Vini123t <vinicius.alm@edu.unipar.br>
2023-06-13 20:09:47 -0300

    atividade

---
+++ Atividades/ex2Pesquisa/src/main/java/br/unipar/ex2pesquisa/Ex2Pesquisa.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package br.unipar.ex2pesquisa;
+
+/**
+ *
+ * @author vinid
+ */
+public class Ex2Pesquisa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- SistemaBancario/src/main/java/br/unipar/sistemabancario/Model/ContaBancaria.java
+++ SistemaBancario/src/main/java/br/unipar/sistemabancario/Model/ContaBancaria.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package br.unipar.sistemabancario.Model;
+
+package br.unipar.sistemabancario.model;
 
 /**
  *
  * @author vinid
  */
 public class ContaBancaria {
+    private int numeroConta;
+    private String nomeTitular;
+    private double saldo;
+    
+    public ContaBancaria(int numeroConta, String nomeTitular, double saldo) {
+        this.numeroConta = numeroConta;
+        this.nomeTitular = nomeTitular;
+        this.saldo = saldo;
+    }
+    
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+    
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+    
+    public double getSaldo() {
+        return saldo;
+    }
+    
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+    
+    public boolean sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
     
 }
